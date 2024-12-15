33c6f7559bfc8188a285818c34762a4032e9d234
Jorge Zetien <jzettien@hotmail.com>
2024-12-15 18:31:43 -0500

    animated container listo

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -28,10 +28,17 @@ final appRouter = GoRouter(
       builder: (context, state) => const ProgressScreen(),
     ),
 
-      GoRoute(
+    GoRoute(
       path: '/snackbars',
       name: SnackbarScreen.name,
       builder: (context, state) => const SnackbarScreen(),
-    )
+    ),
+
+     GoRoute(
+      path: '/animated',
+      name: AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen(),
+    ),
+    
   ]
 );
