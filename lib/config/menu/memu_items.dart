7fcc1664f05218e8e8d56bd528476696fcae02e1
Jorge Zetien <jzettien@hotmail.com>
2024-12-25 22:43:49 -0500

    UI screen Finalizado

--- lib/config/menu/memu_items.dart
+++ lib/config/menu/memu_items.dart
@@ -49,4 +49,10 @@ const appMenuItems = <MenuItem>[
     link: '/animated',
     icon: Icons.check_box_outline_blank_rounded
   ),
+  MenuItem(
+    title: 'UI Controls + Tiles',
+    subTitle: 'Serie de controles de flutter',
+    link: '/ui-controls',
+    icon: Icons.car_rental_outlined
+  ),
 ];
--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -6,6 +6,104 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI Controls'),
+      ),
+      body: const _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView();
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+enum Transportation { car, plane, boat, submarine }
+
+class _UiControlsViewState extends State<_UiControlsView> {
+  bool isDeveloper = true;
+  Transportation selectedTrasport = Transportation.car;
+  bool wantsBreakfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          value: isDeveloper,
+          onChanged: (value) => setState(() {
+            isDeveloper = !isDeveloper;
+          }),
+        ),
+        ExpansionTile(
+          title: const Text('Vehiculo de transporte'),
+          subtitle: Text('$selectedTrasport'),
+          children: [
+            RadioListTile(
+                title: const Text('By car'),
+                subtitle: const Text('Viajar por carro'),
+                value: Transportation.car,
+                groupValue: selectedTrasport,
+                onChanged: (value) => setState(() {
+                      selectedTrasport = Transportation.car;
+                    })),
+            RadioListTile(
+                title: const Text('By plane'),
+                subtitle: const Text('Viajar por avion'),
+                value: Transportation.plane,
+                groupValue: selectedTrasport,
+                onChanged: (value) => setState(() {
+                      selectedTrasport = Transportation.plane;
+                    })),
+            RadioListTile(
+                title: const Text('By boat'),
+                subtitle: const Text('Viajar por barco'),
+                value: Transportation.boat,
+                groupValue: selectedTrasport,
+                onChanged: (value) => setState(() {
+                      selectedTrasport = Transportation.boat;
+                    })),
+            RadioListTile(
+                title: const Text('By submarine'),
+                subtitle: const Text('Viajar por submarino'),
+                value: Transportation.submarine,
+                groupValue: selectedTrasport,
+                onChanged: (value) => setState(() {
+                      selectedTrasport = Transportation.submarine;
+                    }))
+          ],
+        ),
+        CheckboxListTile(
+          title: const Text('¿Desayuno?'),
+          value: wantsBreakfast,
+          onChanged: (value) => setState(() {
+            wantsBreakfast = !wantsBreakfast;
+          }),
+        ),
+        CheckboxListTile(
+          title: const Text('¿Almuerzo?'),
+          value: wantsLunch,
+          onChanged: (value) => setState(() {
+            wantsLunch = !wantsLunch;
+          }),
+        ),
+        CheckboxListTile(
+          title: const Text('¿Cena?'),
+          value: wantsDinner,
+          onChanged: (value) => setState(() {
+            wantsDinner = !wantsDinner;
+          }),
+        )
+      ],
+    );
   }
 }
